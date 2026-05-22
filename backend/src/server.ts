edec5dcbacae64ca8661130f91a50ccf99f5f684
Shubham_maurya2004 <shubhammaurya1578>
2026-05-23 00:51:38 +0530

    fix backend mongodb connection

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -58,11 +58,11 @@ io.on('connection', (socket) => {
 
 const PORT = process.env.PORT || 5000;
 
+connectDB();
+
 if (process.env.NODE_ENV !== "production") {
-  connectDB().then(() => {
-    httpServer.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
+  httpServer.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
   });
 }
 
